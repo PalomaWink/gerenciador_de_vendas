044a004f1617f7ce87adbd2f20bb7d5dc21c537c
Paloma Wink <paloma.wink@hotmail.com>
2023-12-25 22:20:05 -0300

    feat: Inicio da criacao do componente /client

--- src/app/client/page.tsx
+++ src/app/client/page.tsx
@@ -1,7 +1,47 @@
+'use Client';
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+
 export default function Client() {
     return (
-      <main>
-        <h1>Client</h1>
+      <main className="h-screen flex flex-col gap-10 items-center justify-center">
+        <form className="w-full max-w-7xl flex justify-center">
+          <div>
+            <input
+              className="border-black border-2 mr-2 rounded-xl p-2 shadow-lg"
+              type="text" 
+              placeholder="Nome do cliente"
+              
+            />
+          </div>
+          <div>
+            <input
+              className="border-black border-2 mr-2 rounded-xl p-2 shadow-lg"
+              type="number" 
+              placeholder="Valor pago"
+              
+            />
+          </div>
+          <div>
+            <input 
+              className="border-black border-2 mr-2 rounded-xl p-2 shadow-lg"
+              type="date" 
+              placeholder="Data de pagamento"
+              
+            />
+          </div>
+          <div>
+            <input
+              className="border-black border-2 mr-2 rounded-xl p-2 shadow-lg"
+              type="number" 
+              placeholder="Valor devido"
+              
+            />
+          </div>
+        </form>
       </main>
     )
 }
