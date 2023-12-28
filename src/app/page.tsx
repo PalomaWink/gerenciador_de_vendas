ddaef9609a7669547f5bc45ef4dc267b21d4f795
Paloma Wink <paloma.wink@hotmail.com>
2023-12-28 17:22:13 -0300

    feat: Criadas rotas para navegacao da pagina principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main>
       <h1>Home</h1>
+      <Link href="/client" className='mr-8'>Client</Link>
+      <Link href="/admin" className='mr-8'>Admin</Link>
+      <Link href="/recipe">Lista de ingredientes</Link>
     </main>
   )
 }
